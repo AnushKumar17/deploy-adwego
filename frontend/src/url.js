ebab9684196f7cdc8d8520ea7d441ca7346bdb5e
Anush Kumar <116365814+AnushKumar17@users.noreply.github.com>
2024-02-29 16:32:12 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL="http://localhost:5000"
-export const IF = "http://localhost:5000/images/"
+export const URL="https://deploy-adwego.vercel.app"
+export const IF = "https://deploy-adwego.vercel.app/images/"
